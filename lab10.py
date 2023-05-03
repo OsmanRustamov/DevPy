bd625d5e73b7c926e288cd09053f61a442ec4e72
osman <osmanrustamovbk@gmail.com>
2023-05-04 00:15:04 +0500

    init com

--- lab10.py
+++ lab10.py
@@ -1,7 +1,21 @@
 def task1(nums1, nums2):
-    nums = set()
-    nums.update(nums1, nums2)
-    return nums
+    unique_numbers = list(map(str, nums1.union(nums2)))
+    common_numbers = sorted(list(map(str, nums1.intersection(nums2))))
+    return f"unique_numbers = {' '.join(unique_numbers)}\ncommon_numbers = {' '.join(common_numbers)}"
+print(task1({1, 2, 3, 4}, {4, 5, 6, 7}))
 
-print(task1({1, 2, 3, 7}, {4, 5, 6}))
+def task2(line):
+    punctuations = set()
+    letters = set()
 
+    for char in line:
+        if char in ".,;:?!-":
+            punctuations.add(char)
+        elif char >= "E" and char <= "N":
+            letters.add(char)
+
+    result = punctuations.union(letters)
+
+    return f"Множество знаков препинания и букв от E до N: {result}"
+
+print(task2())
